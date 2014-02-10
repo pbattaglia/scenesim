581ff2f06481f1b60a1eefa77a165c2271ca07a5
Peter Battaglia <pbatt@mit.edu>
2014-02-10 11:41:18 -0500

    hotfix windows path seps

--- scenesim/__init__.py
+++ scenesim/__init__.py
@@ -5,6 +5,6 @@ from path import path
 # The root local path.
 ROOT_PATH = path(__path__[0]).abspath().split()[0]
 # Load scenesim-specific Panda3d settings.
-config_pth = path.joinpath(ROOT_PATH, "cfg/Config.prc")
+config_pth = path.joinpath(ROOT_PATH, "cfg", "Config.prc")
 if config_pth.isfile():
     loadPrcFile(config_pth)
