a4154655a23cbb5cd0dca350293de2a82c47f1df
Peter Battaglia <pbatt@mit.edu>
2014-05-25 12:18:40 -0400

    updated setup.py's install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 
 root_pkg = "scenesim"
 sub_pkgs = ["cfg", "display", "objects", "physics", "tests", "util"]
 packages = [root_pkg] + ["%s.%s" % (root_pkg, sub_pkg) for sub_pkg in sub_pkgs]
-
 
 setup(name="scenesim",
       version="0.3",
@@ -14,5 +13,13 @@ setup(name="scenesim",
       author_email="pbatt@mit.edu",
       url="http://scenesim.org",
       packages=packages,
-      requires=["Polygon2", "networkx", "numpy", "panda3d", "path.py"],
+      install_requires=["Sphinx>=1.1.3",
+                        "argparse>=1.2.1",
+                        "networkx>=1.8.1",
+                        "numpy>=1.8.0",
+                        "numpydoc>=0.4",
+                        "path.py>=5.0",
+                        "sphinxcontrib-napoleon>=0.2.1",
+                        "https://bitbucket.org/jraedler/polygon2/downloads/Polygon2-2.0.6.zip",
+                    ],
       provides=["scenesim"])
